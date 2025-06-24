3ce030d37db1642d7f78f24832027aad0ea34586
Vaibhav Kharate <vaibhav.s.kharate@gmail.com>
2025-06-24 11:14:34 +0530

    add 3 modules and final app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 // App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AOS from 'aos';
+import { useEffect } from "react";
+import 'aos/dist/aos.css';
+
 import Weather from "./components/Weather";
 import CropRecommendation from "./components/CropRecommendation";
 import Navbar from "./components/Navbar";
 import MandiPrices from './components/MandiPrices';
-
+import SarkariSchemes from "./pages/SarkariSchemes";
+import KisaanNews from "./pages/KisaanNews";
+import Contact from "./pages/Contact";
 
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 800, once: true });
+  }, []);
+
   return (
     <Router>
       <Navbar />
@@ -14,6 +24,9 @@ function App() {
         <Route path="/" element={<Weather />} />
         <Route path="/recommend" element={<CropRecommendation />} />
         <Route path="/mandiPrices" element={<MandiPrices />} />
+        <Route path="/schemes" element={<SarkariSchemes />} />
+        <Route path="/news" element={<KisaanNews />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
