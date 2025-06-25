b35b0c95220568c2e5882984bb7015c1c65241bd
Vaibhav Kharate <vaibhav.s.kharate@gmail.com>
2025-06-25 22:12:39 +0530

    trying to implement the Multi Language Support to the app

---
+++ src/i18n.js
@@ -0,0 +1,24 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import translationEN from './locales/en/translation.json';
+import translationHI from './locales/hi/translation.json';
+import translationMR from './locales/mr/translation.json';
+
+const resources = {
+  en: { translation: translationEN },
+  hi: { translation: translationHI },
+  mr: { translation: translationMR }
+};
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources,
+    fallbackLng: 'en',
+    interpolation: { escapeValue: false }
+  });
+
+export default i18n;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './i18n';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    
+
   </React.StrictMode>,
 )
